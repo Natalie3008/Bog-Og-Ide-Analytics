5ec31cdea3a90bc7db222cef1975314b645e19f5
FloraCh <79191287+FloraCh@users.noreply.github.com>
2021-04-23 10:57:03 +0200

    Game class

---
+++ src/modelLayer/Game.java
@@ -0,0 +1,22 @@
+package modelLayer;
+
+public class Game 
+{
+	
+	private String type;
+	
+	public Game(String type)
+	{
+		this.type = type;
+	}
+
+	public String getType() 
+	{
+		return type;
+	}
+
+	public void setType(String type) 
+	{
+		this.type = type;
+	}
+}
