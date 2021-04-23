44318f4a52cafd1d15595a8389793dd9f3cca01a
maleksand <1086321@ucn.dk>
2021-04-23 10:28:09 +0200

    supplier help

---
+++ src/modelLayer/Supplier.java
@@ -0,0 +1,87 @@
+package controlLayer;
+
+public class Supplier {
+	
+	private int CVR;
+	private String name;
+	private String contactPerson;
+	private String address;
+	private String phoneNumber;
+	private String email;
+	private String productCategory;
+	
+	
+	public Supplier(int CVR, String name ,String address, String phoneNumber, String email, String productCategory) {
+		
+		this.CVR = CVR;
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		
+	}
+
+
+	public int getCVR() {
+		return CVR;
+	}
+
+
+	public void setCVR(int cVR) {
+		CVR = cVR;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getContactPerson() {
+		return contactPerson;
+	}
+
+
+	public void setContactPerson(String contactPerson) {
+		this.contactPerson = contactPerson;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
