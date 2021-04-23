a6730c3514f5e467029b181baed3a8ce4c71fdb8
Natalie3008 <1088771@ucn.dk>
2021-04-23 10:12:59 +0200

    added copy classes

---
+++ src/modelLayer/BookCopy.java
@@ -0,0 +1,34 @@
+package modelLayer;
+
+public class BookCopy 
+{
+	private String articleNumber;
+	private Book book;
+	
+	public BookCopy (String articleNumber, Book book)
+	{
+		this.articleNumber = articleNumber;
+		this.book = book;
+	}
+
+	public String getArticleNumber() 
+	{
+		return articleNumber;
+	}
+
+	public void setArticleNumber(String articleNumber) 
+	{
+		this.articleNumber = articleNumber;
+	}
+
+	public Book getBook() 
+	{
+		return book;
+	}
+
+	public void setBook(Book book) 
+	{
+		this.book = book;
+	}
+	
+}
