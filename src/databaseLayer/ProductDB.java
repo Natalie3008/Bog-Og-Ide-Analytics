b71eda39b5b06d094e88d14c03006db922f5f7a5
maleksand <1086321@ucn.dk>
2021-04-28 14:42:42 +0200

    progess here

--- src/databaseLayer/ProductDB.java
+++ src/databaseLayer/ProductDB.java
@@ -4,9 +4,9 @@ import modelLayer.Product;
 import modelLayer.Book;
 import modelLayer.Game;
 import java.sql.*;
+import modelLayer.Supplier;
 
-public class ProductDB 
-{
+public class ProductDB {
 
 	private Product buildObject(ResultSet rs, String selectedType) throws SQLException 
 	{
@@ -19,16 +19,17 @@ public class ProductDB
 			{
 
 				builtProduct = new Book(rs.getString("Title"), rs.getString("barcode"), rs.getDouble("costPrice"),
-						rs.getDouble("recommendedRetailPrice"), rs.getInt("amountInStock"),
+						rs.getDouble("recommendedRetailPrice"), rs.getInt("amountInStock"), 
 						rs.getString("publicationDate"), rs.getString("description"), rs.getDate("receivedInStore"),
-						rs.getString("ISBN"), rs.getString("author"), rs.getString("genre"));	
+						buildSupplier(rs.getInt("supplierCVR")), rs.getString("author"), rs.getString("genre"), rs.getString("ISBN"));	
 				
 			 if (selectedType.equals("Game")) 
 			 {
-					builtProduct = new Book(rs.getString("Title"), rs.getString("barcode"), rs.getDouble("costPrice"),
-							rs.getDouble("recommendedRetailPrice"), rs.getInt("amountInStock"),
+					builtProduct = new Game(rs.getString("Title"), rs.getString("barcode"), rs.getDouble("costPrice"),
+							rs.getDouble("recommendedRetailPrice"), rs.getInt("amountInStock"), 
 							rs.getString("publicationDate"), rs.getString("description"), rs.getDate("receivedInStore"),
-							rs.getString("ISBN"), rs.getString("author"), rs.getString("genre"));
+							buildSupplier(rs.getInt("supplierCVR")), rs.getString("Type"));	 
+					
 				
 			}
 			}
@@ -45,17 +46,41 @@ public class ProductDB
 
 	}
 
+	private Supplier buildSupplier(int supplierCVR) throws SQLException {
+		Supplier builtSupplier = null;
+		String SelectSupplier = String.format("SELECT * FROM Supplier WHERE supplierCVR = '" + supplierCVR + "'");
+		Statement statement = DBConnection.getInstance().getConnection().createStatement();
+		ResultSet rs = statement.executeQuery(SelectSupplier);
+
+		if (rs.next()) {
+			try {
+
+				builtSupplier = new Supplier(rs.getInt("CVR"), rs.getString("name"), rs.getString("contactPerson"),
+						rs.getString("address"), rs.getString("phoneNumber"), rs.getString("email"),
+						rs.getString("productCategory"));
+
+			}
+
+			catch (SQLException e) {
+
+				e.printStackTrace();
+
+			}
+
+		}
+		return builtSupplier;
+	}
+
 	// a method for finding stuff with barcode since we have no article number this
 	// time? maybe?
 
-	public Product findProductInDB(String barcode) throws SQLException 
-	{
+	public Product findProductInDB(String barcode) throws SQLException {
 		Product foundProduct = null;
 
-		try 
-		{
+		try {
 
-			// String SelectProducts = String.format("SELECT * FROM Product WHERE barcode = '"+barcode+"'");
+			// String SelectProducts = String.format("SELECT * FROM Product WHERE barcode =
+			// '"+barcode+"'");
 			// not sure about searching just for products so imma leave it greyed out for
 			// now someone with brain help
 			String SelectBooks = String
@@ -68,29 +93,46 @@ public class ProductDB
 			Statement statement = DBConnection.getInstance().getConnection().createStatement();
 
 			ResultSet rs1 = statement.executeQuery(SelectBooks);
-			if (rs1.next()) 
-			{
+			if (rs1.next()) {
 				foundProduct = buildObject(rs1, "Book");
 
 			}
 
 			ResultSet rs2 = statement.executeQuery(SelectGames);
-			if (rs2.next()) 
-			{
+			if (rs2.next()) {
 				foundProduct = buildObject(rs2, "Game");
 
 			}
 
 		}
 
-		catch (SQLException e) 
-		{
+		catch (SQLException e) {
 
 			e.printStackTrace();
 
 		}
-		
+
 		return foundProduct;
 	}
+
+	// method to make a book and add it into the db // WIP STILL
+
+	/*
+	 * public Product createBook(Book book) throws SQLException {
+	 * 
+	 * 
+	 * String sqlProduct =
+	 * "INSERT INTO Product (barcode, title, costPrice, RRP, amountInStock, publicationDate, supplierCVR, description)"
+	 * + " VALUES( " + book.getBarcode() + ", '" + book.getTitle() + "', '" +
+	 * book.getCostPrice() + "', " + book.getRecommendedRetailPrice() + ", " +
+	 * book.getAmountInStock() + ", " + book.getPublicationDate() + ", '" +
+	 * book.getSupplier().getCVR() + "', " + book.getDescription() + "')";
+	 * 
+	 * String sqlBook = "INSERT INTO Book (barcode, ISBN, author, genre) VALUES( " +
+	 * book.getBarcode() + ", " + book.getISBN() + ", '" + book.getAuthor() + ", " +
+	 * book.getGenre() + "')";
+	 * 
+	 * }
+	 */
 
 }
