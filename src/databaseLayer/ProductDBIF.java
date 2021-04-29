8d009c2aa8c3d564b06b9b262d7a671a33e1a52d
Natalie3008 <1088771@ucn.dk>
2021-04-29 11:18:17 +0200

    added productdbif

--- src/databaseLayer/ProductDBIF.java
+++ src/databaseLayer/ProductDBIF.java
@@ -1,5 +1,16 @@
 package databaseLayer;
 
-public class ProductDBIF {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import modelLayer.Product;
+import modelLayer.Supplier;
+
+public interface ProductDBIF {
+	Product buildObject(ResultSet resultSet, String selectedType) throws SQLException;
+	Supplier buildSupplier(int supplierCVR) throws SQLException;
+	ArrayList<Product> getProductInformation(String barcode) throws SQLException;
+	ArrayList<Product> buildObjects(ResultSet resultSet, String category) throws SQLException;
 
 }
