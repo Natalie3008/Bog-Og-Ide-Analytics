b34641644c4f2dc2ea5c350c2d945db3ba715cb4
maleksand <1086321@ucn.dk>
2021-05-05 10:00:44 +0200

    aaaa

--- src/databaseLayer/SaleDB.java
+++ src/databaseLayer/SaleDB.java
@@ -72,7 +72,7 @@ public class SaleDB implements SaleDBIF {
 
 	}
 
-	// TODO comment aaa
+	// TODO comment aaaaa
 	private Employee buildEmployee(int EmployeeCPR) throws SQLException {
 		Employee builtEmployee = null;
 		String SelectEmployee = String.format("SELECT * FROM Employee WHERE EmployeeCPR = " + EmployeeCPR + "");
