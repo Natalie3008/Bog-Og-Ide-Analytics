9cf9f2a94ab2df7c67c10ba637eebefd98ede200
Natalie3008 <1088771@ucn.dk>
2021-05-17 10:44:25 +0200

    version control

--- src/databaseLayer/EmployeeDB.java
+++ src/databaseLayer/EmployeeDB.java
@@ -1,6 +1,7 @@
 package databaseLayer;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import modelLayer.*;
@@ -9,7 +10,7 @@ public class EmployeeDB {
 
 	// Create an employee in DB
 	public boolean createEmployee(Employee employee, String street, int zip, String city, String country) throws SQLException {
-		String sqlEmployee = "INSERT INTO Employee (CPR, name, street, zip, city, country, phoneNumber, email, position) VALUES(?,?,?,?,?,?,?,?,?)";
+		String sqlEmployee = "INSERT INTO Employee (CPR, name, street, zip, city, country, phoneNumber, email, position, version) VALUES(?,?,?,?,?,?,?,?,?,?)";
 		int resultEmployee = 0;
 		try {
 			DBConnection.getInstance().getConnection().setAutoCommit(false);
@@ -24,6 +25,7 @@ public class EmployeeDB {
 			statementEmployee.setInt(7, employee.getPhoneNumber());
 			statementEmployee.setString(8, employee.getEmail());
 			statementEmployee.setString(9, employee.getPosition());
+			statementEmployee.setInt(10, 0);
 			resultEmployee = statementEmployee.executeUpdate();
 			DBConnection.getInstance().getConnection().commit();
 			statementEmployee.close();
@@ -38,13 +40,16 @@ public class EmployeeDB {
 
 	// Update firstName
 	public Employee updateName(Employee employee) throws SQLException {
-		String sql = "UPDATE Employee SET name = ? WHERE CPR = ?";
+		String sql = "UPDATE Employee SET name = ?, version = ? WHERE CPR = ? AND version = ?";
 		int result = 0;
+		int version = getVersion(employee.getCPR());
 		try {
 			DBConnection.getInstance().getConnection().setAutoCommit(false);
 			PreparedStatement statementEmployee = DBConnection.getInstance().getConnection().prepareStatement(sql);
 			statementEmployee.setString(1, employee.getName());
-			statementEmployee.setLong(2, employee.getCPR());
+			statementEmployee.setInt(2,  version + 1);
+			statementEmployee.setLong(3, employee.getCPR());
+			statementEmployee.setInt(4, version);
 			result = statementEmployee.executeUpdate();
 			DBConnection.getInstance().getConnection().commit();
 			statementEmployee.close();
@@ -60,8 +65,9 @@ public class EmployeeDB {
 	// Update Address
 	public Employee updateAddress(Employee employee, String street, int zip, String city, String country)
 			throws SQLException {
-		String sql = "UPDATE Employee SET street = ?, zip = ?, city = ?, country = ? WHERE CPR = ?";
+		String sql = "UPDATE Employee SET street = ?, zip = ?, city = ?, country = ?, version = ? WHERE CPR = ? AND version = ?";
 		int result = 0;
+		int version = getVersion(employee.getCPR());
 		try {
 			DBConnection.getInstance().getConnection().setAutoCommit(false);
 			PreparedStatement statementEmployee = DBConnection.getInstance().getConnection().prepareStatement(sql);
@@ -69,7 +75,9 @@ public class EmployeeDB {
 			statementEmployee.setInt(2, zip);
 			statementEmployee.setString(3, city);
 			statementEmployee.setString(4, country);
-			statementEmployee.setLong(5, employee.getCPR());
+			statementEmployee.setInt(5, version + 1);
+			statementEmployee.setLong(6, employee.getCPR());
+			statementEmployee.setInt(7, version);
 			result = statementEmployee.executeUpdate();
 			DBConnection.getInstance().getConnection().commit();
 			statementEmployee.close();
@@ -84,13 +92,16 @@ public class EmployeeDB {
 
 	// Update phoneNumber
 	public Employee updatePhoneNumber(Employee employee) throws SQLException {
-		String sql = "UPDATE Employee SET phoneNumber = ? WHERE CPR = ?";
+		String sql = "UPDATE Employee SET phoneNumber = ?, version = ? WHERE CPR = ? AND version = ?";
 		int result = 0;
+		int version = getVersion(employee.getCPR());
 		try {
 			DBConnection.getInstance().getConnection().setAutoCommit(false);
 			PreparedStatement statementEmployee = DBConnection.getInstance().getConnection().prepareStatement(sql);
 			statementEmployee.setInt(1, employee.getPhoneNumber());
-			statementEmployee.setLong(2, employee.getCPR());
+			statementEmployee.setInt(2,  version + 1);
+			statementEmployee.setLong(3, employee.getCPR());
+			statementEmployee.setInt(4,  version);
 			result = statementEmployee.executeUpdate();
 			DBConnection.getInstance().getConnection().commit();
 			statementEmployee.close();
@@ -105,13 +116,16 @@ public class EmployeeDB {
 
 	// Update email
 	public Employee updateEmail(Employee employee) throws SQLException {
-		String sql = "UPDATE Employee SET email = ? WHERE CPR = ?";
+		String sql = "UPDATE Employee SET email = ?, version = ? WHERE CPR = ? AND version = ?";
 		int result = 0;
+		int version = getVersion(employee.getCPR());
 		try {
 			DBConnection.getInstance().getConnection().setAutoCommit(false);
 			PreparedStatement statementEmployee = DBConnection.getInstance().getConnection().prepareStatement(sql);
 			statementEmployee.setString(1, employee.getEmail());
-			statementEmployee.setLong(2, employee.getCPR());
+			statementEmployee.setInt(2,  version + 1);
+			statementEmployee.setLong(3, employee.getCPR());
+			statementEmployee.setInt(4,  version);
 			result = statementEmployee.executeUpdate();
 			DBConnection.getInstance().getConnection().commit();
 			statementEmployee.close();
@@ -126,13 +140,16 @@ public class EmployeeDB {
 
 	// Update Position
 	public Employee updatePosition(Employee employee) throws SQLException {
-		String sql = "UPDATE Employee SET position = ? WHERE CPR = ?";
+		String sql = "UPDATE Employee SET position = ?, version = ? WHERE CPR = ? AND version = ?";
 		int result = 0;
+		int version = getVersion(employee.getCPR());
 		try {
 			DBConnection.getInstance().getConnection().setAutoCommit(false);
 			PreparedStatement statementEmployee = DBConnection.getInstance().getConnection().prepareStatement(sql);
 			statementEmployee.setString(1, employee.getPosition());
-			statementEmployee.setLong(2, employee.getCPR());
+			statementEmployee.setInt(2,  version + 1);
+			statementEmployee.setLong(3, employee.getCPR());
+			statementEmployee.setInt(4,  version);
 			result = statementEmployee.executeUpdate();
 			DBConnection.getInstance().getConnection().commit();
 			statementEmployee.close();
@@ -162,5 +179,23 @@ public class EmployeeDB {
 			throw e;
 		}
 		return result > 1;
+	}
+	
+	private int getVersion(long CPR) throws SQLException {
+		String sqlFindVersion = "SELECT version FROM Employee WHERE CPR = ?";
+		 int version = -1;
+	        try {
+	        	DBConnection.getInstance().getConnection().setAutoCommit(false);
+	        	PreparedStatement statement = DBConnection.getInstance().getConnection().prepareStatement(sqlFindVersion);
+	        	statement.setLong(1, CPR);
+	            ResultSet resultSet = statement.executeQuery();
+	            if (resultSet.next()) {
+	                version = resultSet.getInt(1);
+	            }
+	            DBConnection.getInstance().getConnection().setAutoCommit(true);
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return version;
 	}
 }
