f80618c9e213489fb31d5b5cb049d75dee5a67ab
Robinrh10 <75665560+Robinrh10@users.noreply.github.com>
2021-05-21 10:20:41 +0200

    testAnalytics added

---
+++ src/testing/testAnalytics.java
@@ -0,0 +1,150 @@
+package testing;
+
+import static org.junit.Assert.assertEquals;
+
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import controlLayer.*;
+import databaseLayer.ProductDB;
+import databaseLayer.SaleDB;
+import modelLayer.*;
+
+class testAnalytics {
+	
+	private ProductDB productDB;
+	private SaleDB saleDB;
+	private OrderLine orderLine;
+	private Sale sale;
+	private TargetedCategory targetedCategory;
+	private Employee employee;
+	private Supplier supplier;
+	private Product product;
+	private Book book;
+	private Game game;
+	
+
+	@Before
+	public void setUp() throws Exception {
+	productDB = new ProductDB();
+	saleDB = new SaleDB();
+	orderLine = new OrderLine(sale, 123, product);
+	sale = new Sale(986532);
+	//targetedCategory = new TargetedCategory(333, "Students", 19, 29, "Male", "None");
+	//employee = new Employee(12345678, "Tanya Harding", "Bispensgade 34, 3TV, 9000, Aalborg", 620316941, 
+	//		"tanyaharding@gmail.com", "Student aid");
+	
+	product = new Product("1234");
+	supplier = new Supplier(123456789, "Johnny Supplies", "Martin Smile", "Vesterbro 27",
+			"+45605139782", "smileyMartey@gmail.com", "I give u book");
+	Supplier bookSupplier = new Supplier(123456789, "Johnny Supplies", "Martin Smile", "Vesterbro 27",
+			"+45605139782", "smileyMartey@gmail.com", "I give u book");
+	Supplier gameSupplier = new Supplier(987654321, "Supplier Johnny", "Bob Smile", "Vesterbro 27", "+4560514859",
+			"bobIsInLove@gmail.com", "I give u game");
+	book = new Book("1234", "Spork", 14.4, 55.7, 10, "07/11/2020", "Description of pretty book", "English",
+			bookSupplier, "ABC123", "Foon ", "novel");
+	game = new Game("9876", "Exploding puppies", 150.00, 250.50, 1, "21/11/2020", "description of pretty game",
+			"English", gameSupplier, "puzzle");
+	}
+
+
+	//All parameters are correct
+	@Test
+	public void testProductsAnalytics() {
+		// Arrange
+		ArrayList<Product> getProductsAnalytics = new ArrayList<Product>();
+
+		// Act
+		try {
+			getProductsAnalytics.addAll(saleDB.getProductsAnalytics("Slow", "Book", 2019, 11, 27, 2));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		//Assert
+		assertEquals("Return Bestsellers", getProductsAnalytics.size(), 2019);
+	}
+
+	//Incorrect parameter used - choice
+	@Test
+	public void testIncorrectParameterChoice() {
+		// Arrange
+		ArrayList<Product> foundproduct = new ArrayList<Product>();
+
+		// Act
+		try {
+			foundproduct.addAll(saleDB.getProductsAnalytics("No Sale", "Book", 2019, 11, 27, 2));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		//Assert
+		assertEquals("Return empty list", foundproduct.get(0));
+	}
+	
+	//Incorrect parameter used - time
+	@Test
+	public void testIncorrectParameterTime() {
+		// Arrange
+		ArrayList<Product> foundproduct = new ArrayList<Product>();
+
+		// Act
+		
+
+		//Assert
+		
+	}
+
+	//Incorrect targetedCategoryID - not in the database
+	@Test
+	public void testNotInDatabase() {
+		// Arrange
+		ArrayList<Product> foundproduct = new ArrayList<Product>();
+		
+		// Act
+					
+		//Assert
+					
+	}
+	
+	
+	//Incorrect targetedCategoryID - null
+	@Test
+	public void testIncorrectTargetedCategoryID () {
+		// Arrange
+		ArrayList<Product> foundproduct = new ArrayList<Product>();
+		
+		// Act
+					
+		//Assert
+					
+	}
+	
+	//One to test if it works for getSalesAnalytics() 
+	@Test
+	public void testSalesAnalytics() {
+		// Arrange
+		ArrayList<Product> foundproducts = new ArrayList<Product>();
+		
+
+		// Act
+		
+
+		//Assert
+		assertEquals("Return Bestsellers", foundproducts.size());
+		}
+	
+
+		
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+}
