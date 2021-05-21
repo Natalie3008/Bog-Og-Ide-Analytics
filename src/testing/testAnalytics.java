d725106779f8e9b15c19d4700b35a59a0c8fd985
Natalie3008 <1088771@ucn.dk>
2021-05-21 10:35:50 +0200

    testing changes

--- src/testing/testAnalytics.java
+++ src/testing/testAnalytics.java
@@ -16,7 +16,7 @@ import databaseLayer.ProductDB;
 import databaseLayer.SaleDB;
 import modelLayer.*;
 
-class testAnalytics {
+public class testAnalytics {
 	
 	private ProductDB productDB;
 	private SaleDB saleDB;
@@ -40,17 +40,19 @@ class testAnalytics {
 	//employee = new Employee(12345678, "Tanya Harding", "Bispensgade 34, 3TV, 9000, Aalborg", 620316941, 
 	//		"tanyaharding@gmail.com", "Student aid");
 	
-	product = new Product("1234");
-	supplier = new Supplier(123456789, "Johnny Supplies", "Martin Smile", "Vesterbro 27",
-			"+45605139782", "smileyMartey@gmail.com", "I give u book");
-	Supplier bookSupplier = new Supplier(123456789, "Johnny Supplies", "Martin Smile", "Vesterbro 27",
-			"+45605139782", "smileyMartey@gmail.com", "I give u book");
-	Supplier gameSupplier = new Supplier(987654321, "Supplier Johnny", "Bob Smile", "Vesterbro 27", "+4560514859",
-			"bobIsInLove@gmail.com", "I give u game");
-	book = new Book("1234", "Spork", 14.4, 55.7, 10, "07/11/2020", "Description of pretty book", "English",
-			bookSupplier, "ABC123", "Foon ", "novel");
-	game = new Game("9876", "Exploding puppies", 150.00, 250.50, 1, "21/11/2020", "description of pretty game",
-			"English", gameSupplier, "puzzle");
+//	product = new Product("1234");
+//	supplier = new Supplier(123456789, "Johnny Supplies", "Martin Smile", "Vesterbro 27",
+//			"+45605139782", "smileyMartey@gmail.com", "I give u book");
+//	Supplier bookSupplier = new Supplier(123456789, "Johnny Supplies", "Martin Smile", "Vesterbro 27",
+//			"+45605139782", "smileyMartey@gmail.com", "I give u book");
+//	Supplier gameSupplier = new Supplier(987654321, "Supplier Johnny", "Bob Smile", "Vesterbro 27", "+4560514859",
+//			"bobIsInLove@gmail.com", "I give u game");
+//	book = new Book("1234", "Spork", 14.4, 55.7, 10, "07/11/2020", "Description of pretty book", "English",
+//			bookSupplier, "ABC123", "Foon ", "novel");
+//	game = new Game("9876", "Exploding puppies", 150.00, 250.50, 1, "21/11/2020", "description of pretty game",
+//			"English", gameSupplier, "puzzle");
+	
+//The data that is used in these tests has been inserted into the database using the testData sql scripts
 	}
 
 
@@ -58,46 +60,52 @@ class testAnalytics {
 	@Test
 	public void testProductsAnalytics() {
 		// Arrange
-		ArrayList<Product> getProductsAnalytics = new ArrayList<Product>();
+		ArrayList<Product> foundProducts = new ArrayList<Product>();
 
 		// Act
 		try {
-			getProductsAnalytics.addAll(saleDB.getProductsAnalytics("Slow", "Book", 2019, 11, 27, 2));
+			foundProducts.addAll(saleDB.getProductsAnalytics("Slow", "Book", 0, 0, 0, 2));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
 		//Assert
-		assertEquals("Return Bestsellers", getProductsAnalytics.size(), 2019);
+		System.out.println("Size of bestsellers" + foundProducts.size());
+		assertEquals("Returns Bestsellers", foundProducts.size(), 0);
 	}
 
 	//Incorrect parameter used - choice
 	@Test
 	public void testIncorrectParameterChoice() {
 		// Arrange
-		ArrayList<Product> foundproduct = new ArrayList<Product>();
+		ArrayList<Product> foundProducts = new ArrayList<Product>();
 
 		// Act
 		try {
-			foundproduct.addAll(saleDB.getProductsAnalytics("No Sale", "Book", 2019, 11, 27, 2));
+			foundProducts.addAll(saleDB.getProductsAnalytics("No Sale", "Book", 0, 0, 0, 2));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
 		//Assert
-		assertEquals("Return empty list", foundproduct.get(0));
+		assertEquals("Returns an empty list", foundProducts.get(0));
 	}
 	
 	//Incorrect parameter used - time
 	@Test
 	public void testIncorrectParameterTime() {
 		// Arrange
-		ArrayList<Product> foundproduct = new ArrayList<Product>();
+		ArrayList<Product> foundProducts = new ArrayList<Product>();
 
 		// Act
-		
+		try {
+			foundProducts.addAll(saleDB.getProductsAnalytics("", "Book", 2000, 0, 0, 2));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 		//Assert
+		assertEquals("Returns an empty list", foundProducts.get(0));
 		
 	}
 
@@ -105,11 +113,17 @@ class testAnalytics {
 	@Test
 	public void testNotInDatabase() {
 		// Arrange
-		ArrayList<Product> foundproduct = new ArrayList<Product>();
-		
-		// Act
-					
-		//Assert
+				ArrayList<Product> foundProducts = new ArrayList<Product>();
+
+				// Act
+				try {
+					foundProducts.addAll(saleDB.getProductsAnalytics("Fast", "Book", 2000, 0, 0, 315));
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+				//Assert
+				assertEquals("Returns an empty list", foundProducts.get(0));
 					
 	}
 	
@@ -118,11 +132,17 @@ class testAnalytics {
 	@Test
 	public void testIncorrectTargetedCategoryID () {
 		// Arrange
-		ArrayList<Product> foundproduct = new ArrayList<Product>();
-		
+		ArrayList<Product> foundProducts = new ArrayList<Product>();
+
 		// Act
-					
+		try {
+			foundProducts.addAll(saleDB.getProductsAnalytics("Fast", "Book", 2000, 0, 0, -1)); //by default in the UI, when a targeted category
+		} catch (SQLException e) {															//has not been selected, it sets the ID to -1
+			e.printStackTrace();
+		}
+
 		//Assert
+		assertEquals("Returns an empty list", foundProducts.get(0));
 					
 	}
 	
@@ -130,14 +150,19 @@ class testAnalytics {
 	@Test
 	public void testSalesAnalytics() {
 		// Arrange
-		ArrayList<Product> foundproducts = new ArrayList<Product>();
+		ArrayList<OrderLine> foundOrderLines = new ArrayList<OrderLine>();
 		
 
 		// Act
-		
+		try {
+			foundOrderLines.addAll(saleDB.getSalesAnalytics());
+		} catch (SQLException e) {															
+			e.printStackTrace();
+		}
 
 		//Assert
-		assertEquals("Return Bestsellers", foundproducts.size());
+		System.out.println("size of salesAnalytics" + foundOrderLines.size());
+		assertEquals("Return Bestsellers", foundOrderLines.size(), 0);
 		}
 	
 
